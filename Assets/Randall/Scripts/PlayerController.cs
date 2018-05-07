688d8d77bc2f1319ae7fd11d5438fd74ff7840a9
Randall Hines <randallhines0@gmail.com>
2018-05-07 14:18:04 -0700

    -Player edits

--- Assets/Randall/Scripts/PlayerController.cs
+++ Assets/Randall/Scripts/PlayerController.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+    Camera currentCamera;
     public Rigidbody2D rb2D;
     public Vector2 directionalInput;
     public float speed;
 
     // Use this for initialization
     void Start () {
-
+        currentCamera = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera> ();
     }
 
     // Update is called once per frame
@@ -21,5 +22,12 @@ public class PlayerController : MonoBehaviour {
     void GetInput () {
         directionalInput.x = Input.GetAxisRaw ("Horizontal");
         directionalInput.y = Input.GetAxisRaw ("Vertical");
+
+        // Vector3 newRotation = transform.eulerAngles;
+        // newRotation.z  = Vector2.SignedAngle(transform.right,currentCamera.ScreenToWorldPoint (Input.mousePosition));
+        // transform.eulerAngles = newRotation;
+        // Debug.Log(newRotation);
+        
+        //
     }
 }
