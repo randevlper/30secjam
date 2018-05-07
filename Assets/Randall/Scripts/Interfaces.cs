8a7d80f403a3f7029c0032afb9e793c904bbcfcb
Randall Hines <randallhines0@gmail.com>
2018-05-07 11:19:04 -0700

    -Added movement and gun

---
+++ Assets/Randall/Scripts/Interfaces.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct HitData
+{
+	float damage;
+	GameObject other;
+
+	public HitData(GameObject you, float dam)
+	{
+		damage = dam;
+		other = you;
+	}
+}
+public interface IDamageable
+{
+	void Damage(HitData hit);
+}
---
+++ Assets/Randall/Scripts/Utilities/ObjectPoolComponent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gold;
+using Gold.Delegates;
+
+public class ObjectPoolComponent : MonoBehaviour {
+	private ObjectPool objectPool;
+	private bool isSetup;
+	private ValueChange<GameObject> Add;
+
+	//Make sure to set the gameObject to inactive before calling this
+	public bool Setup(ObjectPool pool, ValueChange<GameObject> del)
+	{
+		if(!isSetup)
+		{
+			Add = del;
+			objectPool = pool;
+			isSetup = true;
+			return true;
+		}
+		return false;
+	}
+
+	private void OnDisable()
+	{
+		//Pass in object to a delagate
+		Add(gameObject);
+	}
+
+}
